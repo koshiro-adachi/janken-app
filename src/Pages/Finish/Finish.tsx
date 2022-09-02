3a073fde3d7365518b8726e3cd50ef74957a88df
koshiro-adachi <koushiro54600404@gmail.com>
2022-09-02 16:33:55 +0900

    運勢を追加

--- src/Pages/Finish/Finish.tsx
+++ src/Pages/Finish/Finish.tsx
@@ -1,6 +1,7 @@
 import { FC, useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import "./finish.css";
+import { fortune } from "./fortune";
 
 type CustomizedState = {
   maxWinCount: number;
@@ -45,11 +46,14 @@ export const Finish: FC = () => {
         <div className="resultWrapper1">
           <h1 className="resultTitle">結果</h1>
           <div className="resultMain">
-            <h2 className="resultMessage">お疲れさまでした</h2>
             <div className="pointResult">
               <h3>{`total Point: ${totalPoint + betPoint}point`}</h3>
-              <h3>{`最大連続勝利回数: ${maxWinCount}回${frequency}試合`}</h3>
+              <h3>{`最大連続勝利回数: ${maxWinCount}回`}</h3>
             </div>
+            <h2 className="resultMessage">{`あなたの運勢は${fortune(
+              totalPoint,
+              maxWinCount
+            )}です。`}</h2>
             <div className="resultButtonWrapper">
               <button
                 onClick={onClickVS}
